1446731a6ffccbe877a80eb8239f8ad921b28f67
ianes1978 <roberto.ianes.78@gmail.com>
2021-04-04 23:51:36 +0200

    .

---
+++ src/components/ex-emitter copy/ex-emitter-child.ts
@@ -0,0 +1,32 @@
+import { LitElement, html, css, property, customElement } from "lit-element";
+import { globalStyle } from "../../core/decorators";
+
+
+@customElement("ex-emitter-child")
+class ExEmitterChild extends LitElement {
+  @globalStyle()
+  static get styles() {
+    return [css`
+          :host{
+              display: block;
+          }
+    `];
+  }
+  @property({ type: String }) valueCustom = "";
+
+
+  render() {
+    return html`
+                <h5>child that emit to his parent :</h5>
+                <div class="d-flex">
+                  <input style="width:200px" type="text" class="form-control" value="${this.valueCustom}"
+                    @keydown="${this.emitterMethod}">
+                
+                </div>
+    `;
+  }
+
+  emitterMethod(e) {
+    this.dispatchEvent(new CustomEvent('changeValue', { detail: { value: e.target.value } }));
+  }
+}
